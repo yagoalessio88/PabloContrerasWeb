9db4e04647fefbdad0069d35792e1094533a984b
yagoalessio88 <yagoalessio88@gmail.com>
2021-12-04 00:46:40 +0100

    navBar anchors functionality

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
+//navBar functionality
 const navBar = document.querySelector(".navBar");
 const navLinks = document.querySelector(".nav-links");
+
+const showNavLinks = () => {
+  navLinks.classList.remove("hide-nav");
+};
+
+const hideNavLinks = () => {
+  navLinks.classList.add("hide-nav");
+};
+
+const smoothScrollTo = (id) => {
+  const fixedNavbarHeight = document.querySelector(".nav-center").clientHeight;
+
+  const targetedSection = document.getElementById(id);
+
+  let position = targetedSection.offsetTop - fixedNavbarHeight;
+  hideNavLinks();
+  window.scrollTo({
+    left: 0,
+    // top: element.offsetTop,
+    top: position,
+    behavior: "smooth",
+  });
+};
+
 const toggleNavLinks = (e) => {
-  console.log(e.target.parentElement);
   if (e.target.parentElement.classList.contains("nav-toggle")) {
-    navLinks.classList.remove("hide-nav");
+    showNavLinks();
   }
   if (e.target.parentElement.classList.contains("nav-close")) {
-    navLinks.classList.add("hide-nav");
+    hideNavLinks();
+  }
+  if (e.target.parentElement.classList.contains("anchor-link")) {
+    const id = e.target.parentElement.dataset.id;
+    smoothScrollTo(id);
   }
 };
 navBar.addEventListener("click", toggleNavLinks);
