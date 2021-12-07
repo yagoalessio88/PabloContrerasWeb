ae549fd7761301921bd4639c325e98bc5ab6d194
yagoalessio88 <yagoalessio88@gmail.com>
2021-12-07 02:56:35 +0100

    javascript data module

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+import { shortBio, longBio, shortAlbum, longAlbum } from "./js/data.js";
 //navBar functionality
 const navBar = document.querySelector(".navBar");
 const navLinks = document.querySelector(".nav-links");
 const mainSections = document.querySelector(".main-sections");
+const heroSection = document.querySelector(".main-hero");
 
 const showNavLinks = () => {
   navLinks.classList.remove("hide-nav");
@@ -10,22 +12,19 @@ const showNavLinks = () => {
 const hideNavLinks = () => {
   navLinks.classList.add("hide-nav");
 };
-
+///smooth scroll function
 const smoothScrollTo = (id) => {
   const fixedNavbarHeight = document.querySelector(".nav-center").clientHeight;
-
   const targetedSection = document.getElementById(id);
-
   let position = targetedSection.offsetTop - fixedNavbarHeight;
   hideNavLinks();
   window.scrollTo({
     left: 0,
-    // top: element.offsetTop,
     top: position,
     behavior: "smooth",
   });
 };
-
+/////nav links toggle
 const toggleNavLinks = (e) => {
   if (
     e.target.parentElement.classList.contains("nav-toggle") ||
@@ -47,6 +46,7 @@ const toggleNavLinks = (e) => {
 
 //event listeners for navbar toggle
 navBar.addEventListener("click", toggleNavLinks);
+heroSection.addEventListener("click", hideNavLinks);
 mainSections.addEventListener("click", hideNavLinks);
 
 ///////////////////////////////////////////////////////////////////////////////////
@@ -55,69 +55,22 @@ mainSections.addEventListener("click", hideNavLinks);
 const bioTextContainer = document.querySelector(".bio-text-container");
 const albumTextContainer = document.querySelector(".album-text-container");
 
+///// text toggle functions
+//bio section
 const longBioText = () => {
-  bioTextContainer.innerHTML = `<p class="impair-text">
-            
-El cantautor nacido en Catalunya durante el exilio de su familia, pasó gran parte de su infancia en Barcelona hasta el retorno de sus padres a Mendoza donde descubriría su relación con la música.  <br>
-Es hijo de Antonio Contreras, voz de los grupos Canturía y Markama entre otros, se licenció en Música Popular Latinoamericana en la UNCuyo y ha colaborado para músicos como Horacio Gómez, Juampi Dicesare y Analía Garcetti. <br>
-También formó parte de los grupos Pinto Ensamble, La Levingston Colmenares y Lamoladora, experiencia musical que lo acercó a la música migrante, el folclore y la trova latinoamericana, influencias que coinciden en su cancionero. <br> <br>
-
-Colaboraciones: <br>
-Como Integrante formó parte del Octavio Sánchez Ensamble, el grupo de música brasileña Janela ao sul e integró varias murgas como La Buena Moza y El Remolino entre otras. <br> <br>
-
-Como invitado tocó con agrupaciones como Miles de Años, Parió la Choca, Traful Trío, Pucha Che, El Adagio,Simpecao, Altertango , Grupo de percusión En Clave a Tierra, entre otros. <br> <br>
-
-Grabaciones: <br>
-•Murga La Repicante. <br>
-•Murga La Buena Moza. <br>
-•Álbum "Con aire al sur del mundo" (Simpecao). <br>
-•Álbum "Bailalo como quieras" (La Levingston Colmenares). <br> <br>
-
-Hechos Destacados: <br>
-•Premio Escenario 2007 y 2010 en el rubro Jazz otorgado por el diario Uno de
-Mendoza al Eduardo Pinto Ensamble. <br>
-•Gira Uruguay (festival "Jazz a la calle" - enero 2009 y 2011). <br>
-•Gira Cuba ("Festival del Fuego" - julio de 2013). <br>
-•Pemio "Trajectoria" (setiembre 2013) del 5° Concurso de 
-Música Popular Brasileña "Brasil en Mendoza" y posterior viaje de intercambio a la Escuela Ton Jobim de San Pablo, Brasil.<button class="text-toggle-btn close">...Leer menos</button>
-</p> `;
+  bioTextContainer.innerHTML = longBio;
 };
 const shortBioText = () => {
-  bioTextContainer.innerHTML = `<p class="impair-text">          
-El cantautor nacido en Catalunya durante el exilio de su familia, pasó gran parte de su infancia en Barcelona hasta el retorno de sus padres a Mendoza donde descubriría su relación con la música.  <br>
-Es hijo de Antonio Contreras, voz de los grupos Canturía y Markama entre otros, se licenció en Música Popular Latinoamericana en la UNCuyo y ha colaborado para músicos como Horacio Gómez, Juampi Dicesare y Analía Garcetti. <button class="text-toggle-btn open">Leer más...</button></p> `;
+  bioTextContainer.innerHTML = shortBio;
 };
-
-//////////////////////////////////////album text functions
+//albu section
 const longAlbumText = () => {
-  albumTextContainer.innerHTML = `<p class="pair-text">
-            Primer disco solista donde me animo a explorar el mundo de la canción. Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación. <br>
-            Este material cuenta con la producción musical, dirección y arreglos de Rodrigo Botacaulli y fué grabado entre mayo-julio del 2018 en distintos estudios como:  “Nuestro” Estudio (Buenos Aires), “Las Gracias” Estudio (Buenos Aires), Estudio Páramo (Mendoza) y Estudio de Alejandro Fränkel (Barcelona). Cuenta con las invaluables colaboraciones de: Martín Cappi, Matías García, Ernesto Pérez Matta, Mailén Obrador, Paula Neder, Emilio “Yoya” Jofré, Leo “Fugy” Altavilla, Tito Velazquez y Walter Ulloa .<br><button class="album-text-toggle-btn close">...Leer menos</button>
-            <br /><br>
-            Disponible en las siguientes plataformas:
-          </p>
-          <div class="pair-links">
-            <a target="_blank" href="https://open.spotify.com/album/0XsDuYm5jdxBmGGvXA47SF?si=ynReJMMETG2dFl_nUkDgFQ&nd=1
-            "><i class="fab fa-spotify"></i></a>
-            <a target="_blank" href="https://www.youtube.com/watch?v=xeYCTuCM3aA" 
-              ><i class="fab fa-youtube"></i
-            ></a>
-          </div>`;
+  albumTextContainer.innerHTML = longAlbum;
 };
 const shortAlbumText = () => {
-  albumTextContainer.innerHTML = `<p class="pair-text">
-            Primer disco solista donde me animo a explorar el mundo de la canción. Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación.<br><button class="album-text-toggle-btn open">Leer más...</button>
-            <br /><br>
-            Disponible en las siguientes plataformas:
-          </p>
-          <div class="pair-links">
-            <a target="_blank"
-              href="https://open.spotify.com/album/0XsDuYm5jdxBmGGvXA47SF?si=ynReJMMETG2dFl_nUkDgFQ&nd=1
-            "><i class="fab fa-spotify"></i></a>
-            <a target="_blank" href="https://www.youtube.com/watch?v=xeYCTuCM3aA"><i class="fab fa-youtube"></i></a>
-          </div>`;
+  albumTextContainer.innerHTML = shortAlbum;
 };
-/////////window content loaded
+/////////window content loaded event listener
 window.addEventListener("DOMContentLoaded", () => {
   shortBioText();
   shortAlbumText();
