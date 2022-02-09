b3df291e6999a3542ac144d19b83da6740d9e3bc
yagoalessio88 <yagoalessio88@gmail.com>
2022-02-09 22:26:29 +0100

    color variables corrections

--- js/data.js
+++ js/data.js
@@ -2,7 +2,7 @@
 
 const shortBio = `<p class="impair-text">
                   El cantautor nacido en Catalunya durante el exilio de su familia, pasó gran parte de su infancia en Barcelona hasta el retorno de sus padres a Mendoza donde descubriría su relación con la música.  <br>
-                  Es hijo de Antonio Contreras, voz de los grupos Canturía y Markama entre otros, se licenció en Música Popular Latinoamericana en la UNCuyo y ha colaborado para músicos como Horacio Gómez, Juampi Dicesare y Analía Garcetti. <button class="text-toggle-btn open">Leer más...</button></p> `;
+                  Es hijo de Antonio Contreras, voz de los grupos Canturía y Markama entre otros, se licenció en Música Popular Latinoamericana en la UNCuyo y ha colaborado para músicos como Horacio Gómez, Juampi Dicesare y Analía Garcetti. <button class="text-toggle-btn open">...leer más</button></p> `;
 const longBio = `<p class="impair-text">
                  El cantautor nacido en Catalunya durante el exilio de su familia, pasó gran parte de su infancia en Barcelona hasta el retorno de sus padres a Mendoza donde descubriría su relación con la música.  <br>
                  Es hijo de Antonio Contreras, voz de los grupos Canturía y Markama entre otros, se licenció en Música Popular Latinoamericana en la UNCuyo y ha colaborado para músicos como Horacio Gómez, Juampi Dicesare y Analía Garcetti. <br>
@@ -21,12 +21,12 @@ const longBio = `<p class="impair-text">
                  •Gira Uruguay (festival "Jazz a la calle" - enero 2009 y 2011). <br>
                  •Gira Cuba ("Festival del Fuego" - julio de 2013). <br>
                  •Pemio "Trajectoria" (setiembre 2013) del 5° Concurso de 
-                 Música Popular Brasileña "Brasil en Mendoza" y posterior viaje de intercambio a la Escuela Ton Jobim de San Pablo, Brasil.<button class="text-toggle-btn close">...Leer menos</button>
+                 Música Popular Brasileña "Brasil en Mendoza" y posterior viaje de intercambio a la Escuela Ton Jobim de San Pablo, Brasil.<button class="text-toggle-btn close">...leer menos</button>
                  </p> `;
 const shortAlbum = `<p class="pair-text">
-                     Primer disco solista donde me animo a explorar el mundo de la canción. Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación.<br><button class="album-text-toggle-btn open">Leer más...</button>
+                     Primer disco solista donde me animo a explorar el mundo de la canción. Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación.<br><button class="album-text-toggle-btn open">...leer más</button>
                      <br /><br>
-                     Disponible en las siguientes plataformas:
+                     <span class="italic">Disponible en las siguientes plataformas:</span>
                      </p>
                      <div class="pair-links">
                      <a target="_blank"
@@ -36,9 +36,10 @@ const shortAlbum = `<p class="pair-text">
                      </div>`;
 const longAlbum = `<p class="pair-text">
                    Primer disco solista donde me animo a explorar el mundo de la canción. Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación. <br>
-                   Este material cuenta con la producción musical, dirección y arreglos de Rodrigo Botacaulli y fué grabado entre mayo-julio del 2018 en distintos estudios como:  “Nuestro” Estudio (Buenos Aires), “Las Gracias” Estudio (Buenos Aires), Estudio Páramo (Mendoza) y Estudio de Alejandro Fränkel (Barcelona). Cuenta con las invaluables colaboraciones de: Martín Cappi, Matías García, Ernesto Pérez Matta, Mailén Obrador, Paula Neder, Emilio “Yoya” Jofré, Leo “Fugy” Altavilla, Tito Velazquez y Walter Ulloa .<br><button class="album-text-toggle-btn close">...Leer menos</button>
+                   Este material cuenta con la producción musical, dirección y arreglos de Rodrigo Botacaulli y fué grabado entre mayo-julio del 2018 en distintos estudios como:  “Nuestro” Estudio (Buenos Aires), “Las Gracias” Estudio (Buenos Aires), Estudio Páramo (Mendoza) y Estudio de Alejandro Fränkel (Barcelona). Cuenta con las invaluables colaboraciones de: Martín Cappi, Matías García, Ernesto Pérez Matta, Mailén Obrador, Paula Neder, Emilio “Yoya” Jofré, Leo “Fugy” Altavilla, Tito Velazquez y Walter Ulloa .<br><button class="album-text-toggle-btn close">...leer menos</button>
                    <br /><br>
-                   Disponible en las siguientes plataformas:
+                   <span class="italic">Disponible en las siguientes plataformas:</span>
+                   
                    </p>
                    <div class="pair-links">
                    <a target="_blank" href="https://open.spotify.com/album/0XsDuYm5jdxBmGGvXA47SF?si=ynReJMMETG2dFl_nUkDgFQ&nd=1
