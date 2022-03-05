e92797a745ed187469ed57a7c99d65df791a99c2
yagoalessio88 <yagoalessio88@gmail.com>
2022-03-05 11:23:13 +0100

    text and titles style  correction

--- js/data.js
+++ js/data.js
@@ -6,17 +6,17 @@ const shortBio = `<p class="impair-text">
 const longBio = `<p class="impair-text">
                  El cantautor nacido en Catalunya durante el exilio de su familia, pasó gran parte de su infancia en Barcelona hasta el retorno de sus padres a Mendoza donde descubriría su relación con la música.  <br>
                  Es hijo de Antonio Contreras, voz de los grupos Canturía y Markama entre otros, se licenció en Música Popular Latinoamericana en la UNCuyo y ha colaborado para músicos como Horacio Gómez, Juampi Dicesare y Analía Garcetti. <br>
-                 También formó parte de los grupos Pinto Ensamble, La Levingston Colmenares y Lamoladora, experiencia musical que lo acercó a la música migrante, el folclore y la trova latinoamericana, influencias que coinciden en su cancionero. <br> <br>
-                 Colaboraciones: <br>
-                 Como Integrante formó parte del Octavio Sánchez Ensamble, el grupo de música brasileña Janela ao sul e integró varias murgas como La Buena Moza y El Remolino entre otras. <br> <br>
-                 Como invitado tocó con agrupaciones como Miles de Años, Parió la Choca, Traful Trío, Pucha Che, El Adagio,Simpecao, Altertango , Grupo de percusión En Clave a Tierra, entre otros. <br> <br>
-                 Grabaciones: <br>
-                 •Murga La Repicante. <br>
+                 También formó parte de los grupos Pinto Ensamble, La Levingston Colmenares y Lamoladora, experiencia musical que lo acercó a la música migrante, el folclore y la trova latinoamericana, influencias que coinciden en su cancionero.</p> 
+                 <h2 class="impair-text-title">Colaboraciones:</h2> 
+                 <p class="impair-text"> Como Integrante formó parte del Octavio Sánchez Ensamble, el grupo de música brasileña Janela ao sul e integró varias murgas como La Buena Moza y El Remolino entre otras. <br> <br>
+                 Como invitado tocó con agrupaciones como Miles de Años, Parió la Choca, Traful Trío, Pucha Che, El Adagio,Simpecao, Altertango , Grupo de percusión En Clave a Tierra, entre otros.</p> 
+                 <h2 class="impair-text-title">Grabaciones:</h2> 
+                 <p class="impair-text"> •Murga La Repicante. <br>
                  •Murga La Buena Moza. <br>
                  •Álbum "Con aire al sur del mundo" (Simpecao). <br>
-                 •Álbum "Bailalo como quieras" (La Levingston Colmenares). <br> <br>
-                 Hechos Destacados: <br>
-                 •Premio Escenario 2007 y 2010 en el rubro Jazz otorgado por el diario Uno de
+                 •Álbum "Bailalo como quieras" (La Levingston Colmenares).</p> 
+                 <h2 class="impair-text-title">Hechos Destacados:</h2>
+                 <p class="impair-text"> •Premio Escenario 2007 y 2010 en el rubro Jazz otorgado por el diario Uno de
                  Mendoza al Eduardo Pinto Ensamble. <br>
                  •Gira Uruguay (festival "Jazz a la calle" - enero 2009 y 2011). <br>
                  •Gira Cuba ("Festival del Fuego" - julio de 2013). <br>
@@ -24,7 +24,7 @@ const longBio = `<p class="impair-text">
                  Música Popular Brasileña "Brasil en Mendoza" y posterior viaje de intercambio a la Escuela Ton Jobim de San Pablo, Brasil.<button class="text-toggle-btn close">...leer menos</button>
                  </p> `;
 const shortAlbum = `<p class="pair-text">
-                     Primer disco solista donde me animo a explorar el mundo de la canción. "Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación".<br><button class="album-text-toggle-btn open">...leer más</button>
+                     Primer disco solista donde comienza a explorar el mundo de la canción. "Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación".<br><button class="album-text-toggle-btn open">...leer más</button>
                      <br /><br>
                      <span class="italic">Disponible en las siguientes plataformas:</span>
                      </p>
@@ -35,7 +35,7 @@ const shortAlbum = `<p class="pair-text">
                      <a target="_blank" href="https://www.youtube.com/watch?v=xeYCTuCM3aA"><i class="fab fa-youtube"></i></a>
                      </div>`;
 const longAlbum = `<p class="pair-text">
-                   Primer disco solista donde me animo a explorar el mundo de la canción. "Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación". <br>
+                   Primer disco solista donde comienza a explorar el mundo de la canción. "Consta de 12 tracks, todos de mi autoría, nacidos principalmente en la búsqueda de la sanación". <br>
                    Este material cuenta con la producción musical, dirección y arreglos de Rodrigo Botacaulli y fué grabado entre mayo-julio del 2018 en distintos estudios como:  “Nuestro” Estudio (Buenos Aires), “Las Gracias” Estudio (Buenos Aires), Estudio Páramo (Mendoza) y Estudio de Alejandro Fränkel (Barcelona). Cuenta con las invaluables colaboraciones de: Martín Cappi, Matías García, Ernesto Pérez Matta, Mailén Obrador, Paula Neder, Emilio “Yoya” Jofré, Leo “Fugy” Altavilla, Tito Velazquez y Walter Ulloa .<br><button class="album-text-toggle-btn close">...leer menos</button>
                    <br /><br>
                    <span class="italic">Disponible en las siguientes plataformas:</span>
